d3365d34ac19cc86545d5febe15954b6871b486b
vishal-09-sb <vishalbhandary18@gmail.com>
2023-11-22 14:36:44 +0530

    Added Client
    Created some login HTML and CSS

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import Login from './components/account/Login'
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/account/Login.js
@@ -0,0 +1,64 @@
+import { Box, TextField, Button, styled, Typography } from "@mui/material";
+
+const Component = styled(Box)`
+  width: 400px;
+  margin: auto;
+  box-shadow: 5px 2px 5px 2px rgb(0 0 0/0.6);
+`;
+const Image = styled("img")({
+  width: 100,
+  margin: "auto",
+  display: "flex",
+  padding: "50px 0 0",
+});
+
+const Wrapper = styled(Box)`
+    padding: 25px 35px;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    & > div, & > button, & > p {
+        margin-top: 20px;
+    }
+
+`
+const LoginButton = styled(Button)`
+    text-transform: none;
+    backgound: #FB641B;
+    color: #fff;
+    height: 48px;
+    border-radius: 2px;
+`
+const SignupButton = styled(Button)`
+    text-transform: none;
+    backgound: #fff;
+    color: #2874f0;
+    height: 48px;
+    border-radius: 2px;
+    box-shadow: 0 2px 4px 0 rgb(0 0 0/ 20%);
+
+`
+const Text = styled(Typography)`
+    color: #878787;
+    font-size: 12px;
+`
+const Login = () => {
+  const imageURL =
+    "https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png";
+  return (
+    <Component>
+      <Box>
+        <Image src={imageURL} alt="login" />
+        <Wrapper>
+          <TextField variant="standard" />
+          <TextField variant="standard" />
+          <LoginButton variant="contained">Login</LoginButton>
+          <Text style={{ textAlign: 'center'}}>OR</Text>
+          <SignupButton>Create an account </SignupButton>
+        </Wrapper>
+      </Box>
+    </Component>
+  );
+};
+
+export default Login;
